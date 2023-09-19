5b6b31f2fef5205e2477653588059045b3bec403
ThiagoJM <thiagojm.23@hotmail.com>
2023-09-19 15:38:12 -0300

    Atualizando algumas situações para teste de JS

--- main.js
+++ main.js
@@ -33,10 +33,41 @@ function pegarCor() {
   return corAtual;
 }
 function trocaCor() {
-  const pegeuiCor = pegarCor();
-  if (pegeuiCor == "rgb(248, 240, 229)") {
+  const pegueiCor = pegarCor();
+  if (pegueiCor == "rgb(248, 240, 229)") {
     paragrafo.style.color = "red";
   } else {
     paragrafo.style.color = "white";
   }
 }
+/*const elemento = document.querySelectorAll("a");
+console.log(elemento[2].innerHTML);
+elemento[2].innerHTML = "Realizando um teste";
+console.log(elemento[2].innerHTML);*/
+
+const elementosRede = document.querySelectorAll(".apresentacao__links__site");
+const original0 = elementosRede[0].innerHTML;
+const original1 = elementosRede[1].innerHTML;
+const original2 = elementosRede[2].innerHTML;
+function alterarRedes() {
+  if (elementosRede[0].textContent == "Discord") {
+    console.log("teste23");
+    elementosRede[0].innerHTML = original0;
+    elementosRede[1].innerHTML = original1;
+    elementosRede[2].innerHTML = original2;
+  } else {
+    elementosRede[0].innerHTML = "Discord";
+    elementosRede[1].innerHTML = "Pinterest";
+    elementosRede[2].innerHTML = "PS Plus";
+  }
+  /*Testando situações referente a classList.add
+  if (elementosRede[0].classList.contains("strong-flamengo")) {
+    console.log("teste1");
+    elementosRede[0].classList.remove("strong-flamengo");
+  } else {
+    console.log("testeThigas");
+    elementosRede[0].classList.add("strong-flamengo");
+  }*/
+}
+const botaoAlterarRede = document.querySelector(".redes");
+botaoAlterarRede.addEventListener("click", alterarRedes);
